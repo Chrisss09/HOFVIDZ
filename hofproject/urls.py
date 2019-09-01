7e80bc33076e20f87ed45a8bbf09f6687c36b30e
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-01 20:22:57 +0100

    Created a Hall class

--- hofproject/urls.py
+++ hofproject/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('hall-of-fame/create', views.CreateHall.as_view(), name='create_hall'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

a41d0f161212cc8371f89867ecd65ba068c67d71
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-01 20:49:11 +0100

    Created a form and sign up mode

--- halls/views.py
+++ halls/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from .models import Hall
 
 def home(request):
@@ -12,8 +13,20 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
+    def form_valid(self, form):
+        view = super(SignUp, self).form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
+
 class CreateHall(generic.CreateView):
     model = Hall
     fields = ['title']
     template_name = 'halls/create_hall.html'
     success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super(CreateHall, self).form_valid(form)
+        return redirect('home')

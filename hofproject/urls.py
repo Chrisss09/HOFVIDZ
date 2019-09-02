3d69f092b0076528232ccd2d2bb8ae20511feb9c
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-02 19:58:25 +0100

    Created a form class

--- hofproject/urls.py
+++ hofproject/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('dashboard', views.dashboard, name='dashboard'),
+    # Auth
     path('signup', views.SignUp.as_view(), name='signup'),
     path('login', auth_views.LoginView.as_view(), name='login'),
     path('logout', auth_views.LogoutView.as_view(), name='logout'),
@@ -17,6 +18,8 @@ urlpatterns = [
     path('hall-of-fame/<int:pk>', views.DetailHall.as_view(), name='detail_hall'),
     path('hall-of-fame/<int:pk>/update', views.UpdateHall.as_view(), name='update_hall'),
     path('hall-of-fame/<int:pk>/delete', views.DeleteHall.as_view(), name='delete_hall'),
+    # Video
+    path('hall-of-fame/<int:pk>/addvideo', views.add_video, name='add_video'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

1c3db299ac74f4a74f8e120c955fa78577ee97c5
Chris Fowler <chris.fowler73@hotmail.co.uk>
2019-09-02 21:01:58 +0100

    Created form

--- halls/forms.py
+++ halls/forms.py
@@ -8,4 +8,4 @@ class VideoForm(forms.ModelForm):
         labels = {'youtube_id':'YouTube Id'}
 
 class SearchForm(forms.Form):
-    search_term = forms.CharField(max_length=255, label='Search for Videos:')
+    search_term = forms.CharField(max_length=255, label='Search for Videos ')
--- hofproject/settings.py
+++ hofproject/settings.py
@@ -34,13 +34,14 @@ LOGOUT_REDIRECT_URL = 'home'
 # Application definition
 
 INSTALLED_APPS = [
-    'halls.apps.HallsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'halls',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
